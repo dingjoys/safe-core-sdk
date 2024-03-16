56142df3d37f72b1d37001bf170dbbae08288a21
dingjoys <dingjoys@126.com>
2024-03-16 20:33:04 +0800

    confirm

--- playground/api-kit/confirm-transaction.ts
+++ playground/api-kit/confirm-transaction.ts
@@ -14,7 +14,6 @@ interface Config {
 }
 
 const config: Config = {
-  
   CHAIN_ID: BigInt(84532),
   RPC_URL: "https://maximum-spring-daylight.base-sepolia.quiknode.pro/f80c89e1e8f03bdb4eea77aa68bf8546d8862cc5/",
   SIGNER_ADDRESS_PRIVATE_KEY: 'e8bf34d06d398fa2998c1ec84e7e139f920d256eb43f20e8a9939f35f214bd7c',
